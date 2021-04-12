66a1f9e5573625fc516f53067966dc871391e884
LelandJMika <82242493+LelandJMika@users.noreply.github.com>
2021-04-12 15:16:01 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("I've been at Cal Poly for 4 years");
     }
 }
